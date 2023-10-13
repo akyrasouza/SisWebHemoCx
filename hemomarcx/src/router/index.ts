a3e09e6cf277375f30e9e93c4fbd766caaaba31b
akyrasouza <akyrapop@hotmail.com>
2023-10-13 00:49:15 -0300

    teste

--- hemomarcx/src/router/index.ts
+++ hemomarcx/src/router/index.ts
@@ -2,11 +2,13 @@ import { route } from 'quasar/wrappers';
 import {
   createMemoryHistory,
   createRouter,
+  
   createWebHashHistory,
   createWebHistory,
 } from 'vue-router';
 
 import routes from './routes';
+
 
 /*
  * If not building with SSR mode, you can
